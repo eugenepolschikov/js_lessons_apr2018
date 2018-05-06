0b2905d069cfbc08cc764b5226344790c5021b85
eugene polschikov <eugene.polschikov@gmail.com>
2018-05-06 22:18:30 +0300

    todo paper => adjusted.

--- todoPaper/script.js
+++ todoPaper/script.js
@@ -1,1 +1,9 @@
-var todoItems = [];
+let toDoItems = [];
+
+let loadToDos = function () {
+    $.getJSON("todos.json", function (data) {
+        console.log(data);
+        toDoItems = data;
+    });
+};
+loadToDos();
